0e841d0f4103b6d16e329ab43cbe58053b2d2c8f
MustafaMahdi <46289277+MustafaMahdi@users.noreply.github.com>
2023-02-04 21:19:26 +0200

    add task modal

--- src/components/AddNewTask.tsx
+++ src/components/AddNewTask.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import TaskModal from "./TaskModal";
 const AddNewTask: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOpen = () => setIsOpen(true);
+  const handleClose = () => setIsOpen(false);
+
   return (
     <div className="d-flex align-items-center justify-content-center">
-      <a href="#" className="btn btn-primary btn-block">
+      <button onClick={handleOpen} className="btn btn-primary btn-block">
         Add new
-      </a>
+      </button>
+      <TaskModal isOpen={isOpen} handleCloseModal={handleClose} />
     </div>
   );
 };
